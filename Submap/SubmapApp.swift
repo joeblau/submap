a2f9a3c4c3e658c17572767224771fb57f44c2f5
Joe Blau <im@joeblau.com>
2024-05-18 16:22:22 -0700

    Initial Commit

---
+++ Submap/SubmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmapApp.swift
+//  Submap
+//
+//  Created by Joe Blau on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SubmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
