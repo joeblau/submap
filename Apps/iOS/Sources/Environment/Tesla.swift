253d2c34fbf0913f6d55f58da928022c242b4120
Joe Blau <im@joeblau.com>
2024-05-22 21:32:44 -0700

    feat: remove env

---
+++ Apps/iOS/Sources/Environment/Tesla.swift
@@ -0,0 +1,17 @@
+// Tesla.swift
+// Copyright (c) 2024 Submap
+
+import SwiftUI
+
+@Observable class Tesla: NSObject {}
+
+struct TeslaKey: EnvironmentKey {
+    static let defaultValue: Tesla = .init()
+}
+
+extension EnvironmentValues {
+    var tesla: Tesla {
+        get { self[TeslaKey.self] }
+        set { self[TeslaKey.self] = newValue }
+    }
+}
